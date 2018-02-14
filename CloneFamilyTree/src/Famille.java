b8f8b9fce6797a992240517cc4925dd38c180597
teb.med <lamin.teb94@gmail.com>
2018-02-14 16:45:43 +0100

    Last version with GUI

--- CloneFamilyTree/src/Famille.java
+++ CloneFamilyTree/src/Famille.java
@@ -2,171 +2,192 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Scanner;
 
+import com.mxgraph.view.mxGraph;
+
 public class Famille  implements Cloneable{
 	
-	public Famille(String nom)
-	{
-		this.nom_de_famille=nom;
-		this.a = null;
+   public Famille(String nom){
+    this.nom_de_famille=nom;
+	this.a = null;
+   }
+		
+	//clone method
+   public Object clone() throws CloneNotSupportedException{
+      Famille tmp = (Famille)super.clone();
+	  try{
+	     tmp.a = (Arbre_Genialogique) this.a.clone();
+	  } 
+	  catch (Exception e){	
+	     e.printStackTrace();
+	  }
+	  return tmp;
 	}
 	
-	public Famille(String nom,Arbre_Genialogique a)
-	{
-		this.nom_de_famille = nom;
-		this.a=a;
+	public void affichage(){
+	   System.out.println("        Nom de Famille : "+this.nom_de_famille);
+	 //  this.a.affichage();
 	}
 	
-	//clone methods
-	public Object clone() throws CloneNotSupportedException
-	{
-		Famille tmp = (Famille)super.clone();
-		try
-		{
-			tmp.a = (Arbre_Genialogique) this.a.clone();
-		} 
-		catch (Exception e) {
-			
-			e.printStackTrace();
-		}
-		return tmp;
-	}
-	
-	public void affichage()
-	{
-		  Arbre_Genialogique tmp=this.a;
-		  System.out.println("        Nom de Famille : "+this.nom_de_famille);
-		  tmp.affichage();
-	}
-	public void ajouter_personne(String pr)
-	{
+	public void ajouter_personne(String pr,String pére){
 		
-		Arbre_Genialogique personne = new Arbre_Genialogique(pr);
-		Arbre_Genialogique  tmp = this.a;
-		
-		
-		if(tmp != null)
-	       {
-	       System.out.println("entrer le prenom de son pÃ©re");
-	       String pre = s.next();
-	       Arbre_Genialogique p = tmp.trouver_pere(pre);
-	       p.ajouter_enfant(personne);
-	       }
-	       else
-	       {
-	    	   this.a=personne;
-	       }
+       Arbre_Genialogique personne = new Arbre_Genialogique(pr);
+	   Arbre_Genialogique  tmp = this.a;
+	   
+	   if(tmp != null){   
+	      Arbre_Genialogique p = tmp.trouver_pere(pére);
+	      int x = p.enfants.size();
+	      if(x==0){
+	         personne.posx=p.posx-70;
+	     	 personne.posy =p.posy+55;
+	      }
+	      if(x==1){
+	         personne.posx=p.posx;
+	    	 personne.posy =p.posy+55;	
+	      }
+	      if(x==2){
+	    	 personne.posx=p.posx+70;
+	    	 personne.posy =p.posy+55;	
+	      }
+	         p.ajouter_enfant(personne);
+	   }
+	   else{
+	      personne.posx=180;
+		  personne.posy=10;
+	      this.a=personne;
+	   }
 		
 	}
 	
-	class Arbre_Genialogique implements Cloneable
-	{		
-		public Arbre_Genialogique(String prenom)
-		{
-			
-			this.prenom = prenom;
+	public mxGraph dessiner_arbre(mxGraph gr,Object par){
+	    return this.a.dessiner_arbre(gr,par);
+	}
+	
+	class Arbre_Genialogique implements Cloneable{		
+	
+	    public Arbre_Genialogique(String prenom){		
+		    this.prenom = prenom;
 			this.enfants = new ArrayList();
 		}
 		
-		public Arbre_Genialogique trouver_pere(String prenom)
-		{   
-			
-			Arbre_Genialogique tmp=this;
+		public Arbre_Genialogique trouver_pere(String prenom){   
+		    Arbre_Genialogique tmp=this;
 			LinkedList<Arbre_Genialogique> q = new LinkedList<Arbre_Genialogique>();
 			q.addFirst(tmp);
-			while(!(q.isEmpty())) 
-			{
-	           			  
-				  tmp = q.getLast();
-				  if(tmp.prenom.equals(prenom))
-				  {
-					  break; 
-				  }
-				  else
-				  {
-				     for (int i = 0; i < tmp.enfants.size(); i++) 
-				     {	
-					  q.addFirst(tmp.enfants.get(i));
-				     }
-				  
-				     q.removeLast();
-				  }
+			while(!(q.isEmpty())){
+	            tmp = q.getLast();
+				if(tmp.prenom.equals(prenom)){
+				   break; 
+				}
+				else{
+				    for (int i = 0; i < tmp.enfants.size(); i++){	
+					    q.addFirst(tmp.enfants.get(i));
+				    }
+				    q.removeLast();
+				}
 				 
-			  }
-			 return tmp;
-			
-		 }
-		
+			}
+			return tmp;
+	    }
 		
 		//clone methode
-		public Object clone() throws CloneNotSupportedException
-		{
-			Arbre_Genialogique tmp = (Arbre_Genialogique) super.clone();
-			ArrayList<Arbre_Genialogique> ss = new ArrayList<>();
+		public Object clone() throws CloneNotSupportedException{
+            Arbre_Genialogique tmp = (Arbre_Genialogique) super.clone();
+			ArrayList<Arbre_Genialogique> s = new ArrayList<>();
 			//deep copy
-	        try
-	        {
-				//tmp.enfants = (ArrayList<Arbre_Genialogique>) this.enfants.clone();
-				for (int i = 0; i < this.enfants.size(); i++) 
+	        try{ 
+	            //tmp.enfants = (ArrayList<Arbre_Genialogique>) this.enfants.clone();
+	        	for (int i = 0; i < this.enfants.size(); i++) 
 	        	{
-					Arbre_Genialogique t = this.enfants.get(i);
-					ss.add((Arbre_Genialogique) t.clone());
+	        		Arbre_Genialogique t = this.enfants.get(i);
+					s.add((Arbre_Genialogique) t.clone());
 				}
-	        	tmp.enfants = ss;
-				
+	        	tmp.enfants = s;
+			
 			} catch (Exception e) {
 				e.getStackTrace();
 			}
 			return tmp;
 		}
 		
-		
 		//add node
-		public void ajouter_enfant(Arbre_Genialogique enfant)
-		{  
+		public void ajouter_enfant(Arbre_Genialogique enfant){  
 	      this.enfants.add(enfant); 	
 		}
-		
-		
-		//display methode
-		public void  affichage()
+
+		public  mxGraph  dessiner_arbre(mxGraph graph,Object parent)
 		{
-			  Arbre_Genialogique tmp=this;
+			//dessier noeud 
+			  Arbre_Genialogique tmp3=this;
+			  ArrayList<String> list_nom= new ArrayList<>();
+			  ArrayList<Object> list_noeud=new ArrayList<>();
 			  LinkedList<Arbre_Genialogique> q = new LinkedList<Arbre_Genialogique>();
-			  q.addFirst(tmp);
+			  q.addFirst(tmp3);
+			
 			  while(!(q.isEmpty())) 
 			  {
-	             			  
-				  tmp = q.getLast();
-				  System.out.println("   *******   ");  
-				  System.out.println("Prenom : "+tmp.prenom);  
-				  
-			   if(tmp.enfants.size() != 0)
-			   {
-				 System.out.println("Ses enfants :"); 
-	             for (int i = 0; i < tmp.enfants.size(); i++) 
-				 {
-					 System.out.print(tmp.enfants.get(i).prenom+"   --  ");
-				 }
+	              tmp3 = q.getLast();
+				  if(!(list_nom.contains(tmp3.prenom)))
+				  {
+				  Object v1 = graph.insertVertex(parent, null,tmp3.prenom,tmp3.posx,tmp3.posy, 50, 25);
+				  if(!(list_nom.contains(tmp3.prenom))) list_nom.add(tmp3.prenom);
+				  if(!(list_noeud.contains(v1))) list_noeud.add(v1);
+				  }
+				  q.removeLast();
+			      if(tmp3.enfants.size() != 0)
+			      {
+				   for (int i = 0; i < tmp3.enfants.size(); i++) 
+				   {
+				    q.addFirst(tmp3.enfants.get(i));
+				   }
+			      }		 
+			  }
+			  if(list_nom.size()>1)
+			  {  
+			   //dessiner arc
+			   Arbre_Genialogique tmp1=this;
+		
+			   LinkedList<Arbre_Genialogique> q1= new LinkedList<Arbre_Genialogique>();
+			   q1.addFirst(tmp1);
 			  
-				 System.out.println("   ");
-				 for (int i = 0; i < tmp.enfants.size(); i++) 
-				 {
-				   q.addFirst(tmp.enfants.get(i));
-				 }
-			   }
-				 q.removeLast();
+			   while(!(q1.isEmpty())) 
+			   {
+	             			  
+				  tmp1 = q1.getLast();
+				  int index = 0;
+				  int index2=0;
+				
+				  for (int i4 = 0; i4 < list_nom.size(); i4++) 
+				  {
+				   	 if(list_nom.get(i4).equals(tmp1.prenom))  index = i4;
+				  }
+				  if(tmp1.enfants.size() != 0)
+				  {
+					  for (int i6 = 0; i6 < tmp1.enfants.size(); i6++) 
+					  {
+						  String prn = tmp1.enfants.get(i6).prenom;
+						  for (int i8 = 0; i8 < list_nom.size(); i8++) {
+							   	 if(list_nom.get(i8).equals(prn))  index2 = i8;
+							  }
+						  q1.addFirst(tmp1.enfants.get(i6));
+						  graph.insertEdge(parent, null, i6,list_noeud.get(index), list_noeud.get(index2));
+					  }
+				  }
+				  
+				   q1.removeLast();
 				 
 			  }
-		}
-		
+			  }
+			  
+			return graph;
+		 }
 		
 		//attributs
 	    String prenom;
+	    int posx,posy;
 	    public ArrayList<Arbre_Genialogique> enfants;
-	}
+	 }
 
 	//attributs
 	String nom_de_famille;
     Arbre_Genialogique a;
-    Scanner s = new Scanner(System.in);
 }
--- CloneFamilyTree/src/Test.java
+++ CloneFamilyTree/src/Test.java
@@ -1,101 +1,306 @@
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-public class Test {
+import com.mxgraph.swing.mxGraphComponent;
+import com.mxgraph.view.mxGraph;
+
+
+public class Test extends JFrame {
 	
-	static Scanner s = new Scanner(System.in);
-
-    static void menu()
-    {
-    	System.out.println("------Arbre GÃ©nialogique------------"); 
-    	System.out.println("1--creer une nouvelle famille");
-    	System.out.println("2--Ajouter un enfant");
-    	System.out.println("3--Afficher l'arbre gÃ©nialogique");
-    	System.out.println("4--Cloner une famille");
-    	System.out.println("5--afficher la liste des famille");
-    	System.out.println("6--Quitter");
-    }
-	public static void main(String[] args) throws CloneNotSupportedException 
-	{
-		 int ch = 0;
+	public Test(){ 
+		liste_des_familles = new ArrayList<Famille>();
+		this.setLayout(null);
+		p1= new JPanel();
+		p1.setLayout(null);
+		p1.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		p1.setBounds(20, 1, 400, 300);
+		p1.setBackground(Color.pink);
+		this.add(p1);
 		
-		 ArrayList<Famille> liste_des_familles = new ArrayList<>();
-		 do
-		 {
-			 menu();
-			 System.out.println("Donner votre choix");
-			 ch = s.nextInt();
+		bclone_1 = new JButton();
+		bclone_1.setText("cloner");
+		bclone_1.setBounds(150, 310, 100, 30);
+		this.add(bclone_1);
+		
+		p1c= new JPanel();
+		p1c.setLayout(null);
+		p1c.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		p1c.setBounds(20, 345, 400, 300);
+		p1c.setBackground(Color.pink);
+		this.add(p1c);
+		
+		p2= new JPanel();
+		p2.setLayout(null);
+		p2.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		p2.setBounds(450, 1, 400, 300);
+		p2.setBackground(Color.pink);
+		this.add(p2);
+		
+		bclone_2 = new JButton();
+		bclone_2.setText("cloner");
+		bclone_2.setBounds(600, 310, 100, 30);
+		this.add(bclone_2);
+		
+		p2c= new JPanel();
+		p2c.setLayout(null);
+		p2c.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		p2c.setBounds(450, 345, 400, 300);
+		p2c.setBackground(Color.pink);
+		this.add(p2c);
+		
+		p3= new JPanel();
+		p3.setLayout(null);
+		p3.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		p3.setBounds(880, 1, 400, 300);
+		p3.setBackground(Color.pink);
+	    this.add(p3);
+		
+		bclone_3 = new JButton();
+		bclone_3.setText("cloner");
+		bclone_3.setBounds(1030, 310, 100, 30);
+		this.add(bclone_3);
+		
+		p3c= new JPanel();
+		p3c.setLayout(null);
+		p3c.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		p3c.setBounds(880, 345, 400, 300);
+		p3c.setBackground(Color.pink);
+	    this.add(p3c);
+	    
+	    l_nom = new JLabel("Nom :");
+	    l_nom.setBounds(100,670,80,50);
+		this.add(l_nom);
+	    
+	    tf_nom = new JTextField();
+	    tf_nom.setBounds(160,680,150,30);
+	    this.add(tf_nom);
+	    
+	    l_prenom= new JLabel("Prenom :");
+	    l_prenom.setBounds(400,670,80,50);
+		this.add(l_prenom);
+	    
+	    tf_prenom = new JTextField();
+	    tf_prenom.setBounds(480,680,150,30);
+	    this.add(tf_prenom);
+	    
+	    l_père= new JLabel("Pére :");
+	    l_père.setBounds(750,670,80,50);
+		this.add(l_père);
+	    
+	    tf_père = new JTextField();
+	    tf_père.setBounds(810,680,150,30);
+	    this.add(tf_père);
+	    
+	    b_ajouter_person = new JButton("Ajouter");
+	    b_ajouter_person.setBounds(1100,680,100,30);
+	    this.add(b_ajouter_person);
+	    
+	    b_ajouter_person.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				String nom = tf_nom.getText().trim();
+	            String prenom = tf_prenom.getText().trim();
+	            String père = tf_père.getText().trim();
+	      
+	            boolean exist = false;
+	            int i=0;
+	            for (i=0; i < liste_des_familles.size(); i++) {
+					if(liste_des_familles.get(i).nom_de_famille.equals(nom))
+					{
+					  System.out.println(liste_des_familles.get(i));
+					  exist = true;
+					  break;
+					}
+				}
+	            if(!exist)
+	            {
+	            	Famille f = new Famille(nom);
+	 	            liste_des_familles.add(f);
+	            }
+	           
+	          if(i==0) 
+	          {
+	        	p1.removeAll();
+				p1.validate();
+	            liste_des_familles.get(i).ajouter_personne(prenom,père);
+	            liste_des_familles.get(i).affichage();
+	            System.out.println(liste_des_familles.get(i));
+	            lfamille1 = new JLabel(liste_des_familles.get(i).nom_de_famille);
+	            lfamille1.setBounds(5, 5, 120, 20);
+	            lfamille1.setForeground(Color.red);
+	            lfamille1.setFont(lfamille1.getFont().deriveFont(22.0f));
+	            p1.add(lfamille1);
+	            
+	            graph1 = new mxGraph();
+	            parent1 = graph1.getDefaultParent();
+	            
+	            graph1 = liste_des_familles.get(i).dessiner_arbre(graph1,parent1);
+	            graphComponent1 = new mxGraphComponent(graph1);
+			    graphComponent1.setBounds(1, 1, 550, 550);
+			    p1.revalidate();
+			    repaint();
+				p1.add(graphComponent1);
+	          }
+	          else if(i==1) 
+	               {
+	        	     p2.removeAll();
+				     p2.validate();
+	                 liste_des_familles.get(i).ajouter_personne(prenom,père);
+	                 liste_des_familles.get(i).affichage();
+	                 System.out.println(liste_des_familles.get(i));
+	                 lfamille2 = new JLabel(liste_des_familles.get(i).nom_de_famille);
+	 	             lfamille2.setBounds(5, 5, 100, 20);
+	 	             lfamille2.setForeground(Color.red);
+		             lfamille2.setFont(lfamille2.getFont().deriveFont(22.0f));
+	 	             p2.add(lfamille2);
+	 	             
+	 	             graph2 = new mxGraph();
+	 	             parent2 = graph2.getDefaultParent();
+	                 graph2 = liste_des_familles.get(i).dessiner_arbre(graph2,parent2);
+	                 
+	                 graphComponent2 = new mxGraphComponent(graph2);
+			         graphComponent2.setBounds(1, 1, 550, 550);
+			         p2.revalidate();
+			         repaint();
+				     p2.add(graphComponent2);
+	               }
+	               else if(i==2) 
+                        {
+       	                  p3.removeAll();
+			              p3.validate();
+                          liste_des_familles.get(i).ajouter_personne(prenom,père);
+                          liste_des_familles.get(i).affichage();
+                          System.out.println(liste_des_familles.get(i));
+                          lfamille3 = new JLabel(liste_des_familles.get(i).nom_de_famille);
+          	              lfamille3.setBounds(5, 5, 100, 20);
+          	              lfamille3.setForeground(Color.red);
+        	              lfamille3.setFont(lfamille3.getFont().deriveFont(22.0f));
+          	              p3.add(lfamille3);
+                          graph3 = liste_des_familles.get(i).dessiner_arbre(graph3,parent3);
+                          graphComponent3 = new mxGraphComponent(graph3);
+		                  graphComponent3.setBounds(1, 1, 550, 550);
+		                  p3.revalidate();
+		                  repaint();
+			              p3.add(graphComponent3);
+                        }
+	          tf_prenom.setText("  ");
+              tf_nom.setText("  ");
+              tf_père.setText("  ");
+			}	
+		});
+	
+	    bclone_1.addActionListener(new ActionListener()
+	    {
+			public void actionPerformed(ActionEvent arg0) {
+				Famille f1c = null;
+				p1c.removeAll();
+				p1c.validate();
+			    try 
+			    {
+				  f1c = (Famille) liste_des_familles.get(0).clone();
+			    } catch (CloneNotSupportedException e)
+			    {
+				 e.printStackTrace();
+			    }
 			 
-			 switch (ch)
-			 { 
-			  case 1:    System.out.println("Entrer le nom de famille:");
-			             String nom = s.next();
-			             Famille f = new Famille(nom);
-			             liste_des_familles.add(f);
-			             break;
-			            
-			         
-			  case 2:   System.out.println("Entrer le nom :");
-	                    String nomf = s.next();
-	                    System.out.println("Entrer son/sa prenom");
-	                    String prenomf = s.next();
-	                    int i = 0;
-	                    for (i=0; i < liste_des_familles.size(); i++) {
-							if(liste_des_familles.get(i).nom_de_famille.equals(nomf))
-							{
-							  System.out.println(liste_des_familles.get(i));
-							  liste_des_familles.get(i).ajouter_personne(prenomf);
-							}
-						}
-			            break;
-			            
-			   case 3: System.out.println("Entrer le nom de la famille que vous voulez afficher son arbre");
-			           String snom = s.next();
-			           int k = 0;
-			           Famille tmp = null;
-	                    for (k=0; k < liste_des_familles.size(); k++)
-	                    {
-							if(liste_des_familles.get(k).nom_de_famille.equals(snom))
-							{
-								 tmp= liste_des_familles.get(k);
-							}
-						}
-	                    tmp.affichage();
-	                    break;
-	                    
-			   case 4: System.out.println("Entrer le nom de la famille que vous voulez cloner");
-			           String snom1 = s.next();
-	                   int k1 = 0;
-	                   Famille tmp1 = null;
-                       for (k=0; k < liste_des_familles.size(); k++) 
-                       {
-			             if(liste_des_familles.get(k).nom_de_famille.equals(snom1))
-					     {
-						    tmp1= liste_des_familles.get(k);
-					     }
-			           }
-                        Famille ftmp = (Famille) tmp1.clone();
-                        System.out.println("after cloning");
-                        ftmp.affichage();
-			             break;  
-			             
-			   case 5:for (int j12 = 0; j12 < liste_des_familles.size(); j12++)
-		              {
-		                  if(j12<liste_des_familles.size()-1)
-		                  {
-		                    System.out.print(liste_des_familles.get(j12).nom_de_famille+" -- ");
-		                  }
-		                  else
-		                  {
-		        	        System.out.print(liste_des_familles.get(j12).nom_de_famille); 
-		                  }
-		               }
-			           break;
-			   
-			   }
-		 }while(ch==1 || ch==2 || ch==3 || ch==4 || ch==5);
-		 
-		 
+		     graph1c= f1c.dessiner_arbre(graph1c,parent1c);
+			 graphComponent1c = new mxGraphComponent(graph1c);
+		     graphComponent1c.setBounds(1, 1, 550, 550);
+		     p1c.revalidate();
+		     repaint();
+			 p1c.add(graphComponent1c);
+			}
+		});
+	    
+	    bclone_2.addActionListener(new ActionListener()
+	    {
+			public void actionPerformed(ActionEvent arg0) {
+				Famille f2c = null;
+				p2c.removeAll();
+				p2c.validate();
+			    try 
+			    {
+				  f2c = (Famille) liste_des_familles.get(1).clone();
+			    } catch (CloneNotSupportedException e)
+			    {
+				 e.printStackTrace();
+			    }
+			 
+		     graph2c= f2c.dessiner_arbre(graph2c,parent2c);
+			 graphComponent2c = new mxGraphComponent(graph2c);
+		     graphComponent2c.setBounds(1, 1, 550, 550);
+		     p2c.revalidate();
+		     repaint();
+			 p2c.add(graphComponent2c);
+			}
+		});
+	    
+	    bclone_3.addActionListener(new ActionListener()
+	    {
+			public void actionPerformed(ActionEvent arg0) {
+				Famille f3c = null;
+				p3c.removeAll();
+				p3c.validate();
+			    try 
+			    {
+				  f3c = (Famille) liste_des_familles.get(2).clone();
+			    } catch (CloneNotSupportedException e)
+			    {
+				 e.printStackTrace();
+			    }
+			 
+		     graph3c= f3c.dessiner_arbre(graph3c,parent3c);
+			 graphComponent3c = new mxGraphComponent(graph3c);
+		     graphComponent3c.setBounds(1, 1, 550, 550);
+		     p3c.revalidate();
+		     repaint();
+			 p3c.add(graphComponent3c);
+			}
+		});
+	    
+	    
+	    this.setResizable(true);
+		//this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		this.setVisible(true);
 	}
+	
+	public static void main(String args[]){
+		new Test();
+	}
+	
+	 //les attributs
+	 JPanel p1,p2,p3,p1c,p2c,p3c;
+	 JButton bclone_1,bclone_2,bclone_3,b_ajouter_person;
+	 JLabel l_nom, l_prenom , l_père , lfamille1, lfamille2 , lfamille3;
+	 JTextField tf_nom,tf_prenom,tf_père;
+	
+	 mxGraph graph1,graph2;
+	 Object parent1,parent2;
+	 mxGraphComponent graphComponent1,graphComponent2,graphComponent3,graphComponent1c,graphComponent2c,graphComponent3c;
+	 
+	 
+	
+	 
+	 mxGraph graph3 = new mxGraph();
+	 Object parent3 = graph3.getDefaultParent();
+	 
+	 
+	 mxGraph graph1c = new mxGraph();
+	 Object parent1c = graph1c.getDefaultParent();
+	
+	 mxGraph graph2c = new mxGraph();
+	 Object parent2c = graph2c.getDefaultParent();
+	 mxGraph graph3c = new mxGraph();
+	 Object parent3c = graph3c.getDefaultParent();
+	 ArrayList<Famille> liste_des_familles;	 	
 }
