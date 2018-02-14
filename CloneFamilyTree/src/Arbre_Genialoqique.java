aab563e0386843288e46791445e027ea07790107
teb.med <lamin.teb94@gmail.com>
2018-02-14 13:07:28 +0100

    More advanced version

--- CloneFamilyTree/src/Arbre_Genialoqique.java
+++ CloneFamilyTree/src/Arbre_Genialoqique.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
@@ -11,52 +12,62 @@ public class Arbre_Genialoqique implements Cloneable{
 	}
 		
 	//clone methode
-	public Object clone() throws CloneNotSupportedException
-	{
-		Arbre_Genialoqique tmp = (Arbre_Genialoqique) super.clone();	
-		ArrayList<Arbre_Genialoqique> s = new ArrayList<>();
-		//deep copy
-        try
-        {  
-        	/*
-			tmp.enfants = (ArrayList<Arbre_Genialoqique>) this.enfants.clone();
-			*/
-			for (int i = 0; i < this.enfants.size(); i++) 
-        	{
-				Arbre_Genialoqique t = this.enfants.get(i);
-				s.add((Arbre_Genialoqique) t.clone());
-			}
-        	tmp.enfants = s;
-			
-		} catch (Exception e) {
-			e.getStackTrace();
-		}
-		return tmp;
-	}
-		
-	//add node
-	public void ajouter_enfant(Arbre_Genialoqique enfant)
-	{  
-	   this.enfants.add(enfant); 
-	}
-	
-	//display methode
-	public  void  affichage()
-	{
-		 
-
-		System.out.println(this.nom+" addresse physique: "+this);	  
-		while(this != null)
+		public Object clone() throws CloneNotSupportedException
 		{
-	       //display the children's list
-		    for (int i = 0; i < this.enfants.size(); i++) 
-			{
-			  Arbre_Genialoqique tmp = this.enfants.get(i);
-		      System.out.println(tmp.nom+" addresse physique: "+tmp);
+			Arbre_Genialoqique tmp = (Arbre_Genialoqique) super.clone();
+			ArrayList<Arbre_Genialoqique> s = new ArrayList<>();
+			//deep copy
+	        try
+	        {
+				//tmp.enfants = (ArrayList<Arbre_Genialoqiquee>) this.enfants.clone();
+				
+				for (int i = 0; i < this.enfants.size(); i++) 
+	        	{
+					Arbre_Genialoqique t = this.enfants.get(i);
+					s.add((Arbre_Genialoqique) t.clone());
+				}
+	        	tmp.enfants = s;
+				
+			} catch (Exception e) {
+				e.getStackTrace();
 			}
-		  break;
-		  }
+			return tmp;
 		}
+			
+		//add node
+		public void ajouter_enfant(Arbre_Genialoqique enfant)
+		{  
+		   this.enfants.add(enfant); 
+		}
+		
+		//display methode
+		public void  affichage()
+		{
+			 
+			  Arbre_Genialoqique tmp=this;
+			  LinkedList<Arbre_Genialoqique> q = new LinkedList<Arbre_Genialoqique>();
+			  q.addFirst(tmp);
+			  while(!(q.isEmpty())) 
+			  {
+	             			  
+				  tmp = q.getLast();
+				  System.out.println(tmp.nom);  
+				//  System.out.println("le nombre de ses fils est :"+tmp.enfants.size());
+				  System.out.println(tmp);
+				  System.out.println("ses enfants");
+				  for (int i = 0; i < tmp.enfants.size(); i++) 
+				  {
+					System.out.println(tmp.enfants.get(i).nom);
+					System.out.println(tmp.enfants.get(i));
+					q.addFirst(tmp.enfants.get(i));
+					
+				  }
+				  
+				  System.out.println("----------");
+				  q.removeLast();
+				 
+			  }
+			}
 	
 	//attributs
 	String nom;
--- CloneFamilyTree/src/Test.java
+++ CloneFamilyTree/src/Test.java
@@ -1,28 +1,38 @@
 
-public class Test
-{
+public class Test {
+
+	
 	public static void main(String[] args) throws CloneNotSupportedException 
 	{
-		Arbre_Genialoqique a = new Arbre_Genialoqique("MOHAMMED");
-		Arbre_Genialoqique b = new Arbre_Genialoqique("ABDOU"); 
-		Arbre_Genialoqique c = new Arbre_Genialoqique("AMIN"); 
-		a.ajouter_enfant(b);
-		a.ajouter_enfant(c);
+		 Arbre_Genialoqique a = new Arbre_Genialoqique("mohammed");
+		 Arbre_Genialoqique b = new Arbre_Genialoqique("ali"); 
+		 Arbre_Genialoqique c = new Arbre_Genialoqique("amin"); 
+	     Arbre_Genialoqique d = new Arbre_Genialoqique("nabil");
+		 	 		 
+	
+		 a.ajouter_enfant(b);
+		 a.ajouter_enfant(c);
+	     a.ajouter_enfant(d);
+		
+		 Arbre_Genialoqique b1 = new Arbre_Genialoqique("abdou");
+		 Arbre_Genialoqique b2 = new Arbre_Genialoqique("morched");
+		 Arbre_Genialoqique b3 = new Arbre_Genialoqique("khalil");
+	
+		 b.ajouter_enfant(b1);
+		 b1.ajouter_enfant(b2);
+		 b.ajouter_enfant(b3);
 		 
-        a.affichage();
+		 Arbre_Genialoqique b31 = new Arbre_Genialoqique("ayoub");
+		 b3.ajouter_enfant(b31);
+	   
+	
+         a.affichage();	 
          
-		System.out.println("---------------after cloning----------------");
-		Arbre_Genialoqique a1 = (Arbre_Genialoqique) a.clone();
-		Arbre_Genialoqique ck = new Arbre_Genialoqique("Soufien"); 
-		Arbre_Genialoqique h1 = new Arbre_Genialoqique("Khalil"); 
-		Arbre_Genialoqique h2 = new Arbre_Genialoqique("Mounir"); 
-		a1.ajouter_enfant(ck);
-		a1.ajouter_enfant(h1);
-		a1.ajouter_enfant(h2);
-		a1.affichage();
+         Arbre_Genialoqique clo = (Arbre_Genialoqique) a.clone();
+         System.out.println("------------------------------- apres clonage--------------------------------");
+       
+         clo.affichage();
+          
 		 
-		System.out.println("---------------after modifying----------------");
-		a.affichage();
-		 
-    }
+	}
 }
